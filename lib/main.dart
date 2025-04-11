1c272bbe1ec1c9b08dbe4172b6aa192072ef5c21
Rabyn-Collab <babynshrestha64@gmail.com>
2025-04-11 16:57:53 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,33 @@ import 'package:magh/routes/app_routes.dart';
 
 /// provider , notifier provider, future provider, async notifier provider, stream provider
 
+
+// void m (){
+//   for(int i = 0; i<=1000000000;i++){
+//
+//   }
+//   print('sello world');
+// }
+
+Future<String> getSome(){
+  return Future.delayed(Duration(seconds: 2), (){
+    for(int i = 0; i<=1000000000;i++){
+
+    }
+    print('2sec gone');
+    return 'hello world';
+  });
+}
+void a (){
+  print('hello world');
+}
+
+
 void main (){
-
-
+//m();
+ final g = getSome();
+ print(g);
+  a();
 runApp(ProviderScope(child: Home()));
 }
 
